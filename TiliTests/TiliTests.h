e471dbd352c5f2f27146f8289a9bce2e24789d11
Adilet Abylov <a.abylov@gmail.com>
2012-04-18 12:55:28 +0600

    Initial Commit

---
+++ TiliTests/TiliTests.h
@@ -0,0 +1,13 @@
+//
+//  TiliTests.h
+//  TiliTests
+//
+//  Created by Adilet Abylov on 18.04.12.
+//  Copyright (c) 2012 none. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TiliTests : SenTestCase
+
+@end
---
+++ TiliTests/TiliTests.m
@@ -0,0 +1,32 @@
+//
+//  TiliTests.m
+//  TiliTests
+//
+//  Created by Adilet Abylov on 18.04.12.
+//  Copyright (c) 2012 none. All rights reserved.
+//
+
+#import "TiliTests.h"
+
+@implementation TiliTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TiliTests");
+}
+
+@end
